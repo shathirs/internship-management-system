ac31b3592681e1f3be24743d073606f43622c444
shathirs <sheriffshathir@gmail.com>
2026-07-28 11:34:08 +0530

    Create CustomUserDetailService.

---
+++ backend/src/main/java/com/internship/backend/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.internship.backend.security;
+
+import java.util.List;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.internship.backend.model.User;
+import com.internship.backend.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+        return org.springframework.security.core.userdetails.User.builder()
+        .username(user.getEmail())
+        .password(user.getPassword())
+        .authorities(List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())))
+        .build();
+    }
+}
