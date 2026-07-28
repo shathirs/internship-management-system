5ff8f61b4850e061c8002145926aa66d5b558e38
shathirs <sheriffshathir@gmail.com>
2026-07-28 11:01:58 +0530

    Add user getters/setters.

--- backend/src/main/java/com/internship/backend/model/User.java
+++ backend/src/main/java/com/internship/backend/model/User.java
@@ -19,5 +19,53 @@ public class User {
 
     private Role role;
 
-    // getters and setters
+    public User() {
+    }
+
+    public User(String name, String email, String password, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
